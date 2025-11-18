9468a48fdd9c67ebfa1d2d2c39a49e2379d5eed7
tausiq2003 <tausiqsamantaray03@gmail.com>
2025-11-18 15:56:03 +0530

    repl

--- app/main.go
+++ app/main.go
@@ -13,25 +13,33 @@ var _ = os.Stdout
 
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
-	reader := bufio.NewScanner(os.Stdin)
-	reader.Split(bufio.ScanBytes)
-	var typedCmd string
-	for reader.Scan() {
-		tokens := reader.Text()
-		if tokens != "\n" {
-			typedCmd += tokens
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+		// seg: input starts here
 
+		reader := bufio.NewScanner(os.Stdin)
+		reader.Split(bufio.ScanBytes)
+		var typedCmd string
+		for reader.Scan() {
+			tokens := reader.Text()
+			if tokens != "\n" {
+				typedCmd += tokens
+
+			}
+			if tokens == "\n" {
+				break
+			}
+			//fmt.Println(tokens)
 		}
-		if tokens == "\n" {
-			break
+		if err := reader.Err(); err != nil {
+			// Your code here
+			panic(err)
 		}
-		//fmt.Println(tokens)
+
+		if strings.TrimSpace(typedCmd) != "" {
+
+			fmt.Printf("%v: command not found\n", strings.Split(typedCmd, " ")[0])
+		}
 	}
-	if err := reader.Err(); err != nil {
-		// Your code here
-		panic(err)
-	}
-	fmt.Printf("%v: command not found", strings.Split(typedCmd, " ")[0])
 
 }
