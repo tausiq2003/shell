8ee48932ef9858572fb7d8a5802709df0017ba53
tausiq2003 <tausiqsamantaray03@gmail.com>
2025-11-19 15:11:08 +0530

    check path, exec files

--- app/main.go
+++ app/main.go
@@ -3,15 +3,78 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
-	"slices"
 	"strconv"
 	"strings"
 )
 
 // Ensures gofmt doesn't remove the "fmt" and "os" imports in stage 1 (feel free to remove this!)
-var _ = fmt.Fprint
-var _ = os.Stdout
+
+func typeCheck(cmd string) { // only the command
+
+	//	commands := []string{"exit", "type", "echo"}
+	//ignore
+	//	command := strings.Split(cmd, " ")[1]
+	//	if slices.Contains(commands, command) {
+	//		fmt.Printf("%v is a shell builtin\n", command)
+	//	} else {
+	//		fmt.Printf("%v: not found\n", command)
+	//	}
+	// so first take the cmd, compute the path
+	//no first check if the shell-builtin in hash set, O(1)
+	sbmap := map[string]struct{}{"echo": struct{}{}, "[": struct{}{}, "type": struct{}{}, "exit": struct{}{}}
+	_, exists := sbmap[cmd]
+	if exists {
+		fmt.Printf("%v is a shell builtin\n", cmd)
+	} else {
+
+		pathStr := os.Getenv("PATH")
+		pathList := strings.Split(pathStr, ":")
+		// now we have path and cmd, now we have to search for it
+		flag := 0
+		for _, path := range pathList {
+
+			dir, err := os.Open(path)
+			if err != nil {
+				log.Fatal("Error: ", err)
+			}
+			defer dir.Close()
+			files, err := dir.Readdir(-1)
+			if err != nil {
+				log.Fatal("Error: ", err)
+			}
+			for _, file := range files {
+				if file.Name() == cmd {
+
+					if file.Mode()&73 != 0 {
+						/* very important thing, it checks whether the file is executable or not. how?
+						first we have permission bits as follows
+						111 111 111
+						if we & with 73 which is 001001001
+						it will cancel all the bits r & w and return only non zero number
+						if its only 755 like here it would return 73 else 64, 8 or 1
+						*/
+						fmt.Printf("%v is %v\n", cmd, dir.Name())
+						flag = 1
+						break
+
+					}
+				}
+
+			}
+			if flag == 1 {
+				break
+
+			}
+		}
+		if flag == 0 {
+			fmt.Printf("%v not found\n", cmd)
+
+		}
+
+	}
+}
 
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
@@ -47,8 +110,7 @@ func main() {
 			exitCode := strings.Split(cmd, " ")[1]
 			intexitCode, err := strconv.Atoi(exitCode)
 			if err != nil {
-				// Your code here
-				panic(err)
+				log.Fatal("Error", err)
 			}
 			if intexitCode < 0 || intexitCode > 125 {
 				fmt.Println("Invalid exit code")
@@ -63,14 +125,7 @@ func main() {
 
 		}
 		if strings.Split(cmd, " ")[0] == "type" {
-			commands := []string{"exit", "type", "echo"}
-			command := strings.Split(cmd, " ")[1]
-			if slices.Contains(commands, command) {
-				fmt.Printf("%v is a shell builtin\n", command)
-			} else {
-				fmt.Printf("%v: not found\n", command)
-			}
-
+			typeCheck(strings.Split(cmd, " ")[1])
 			continue
 
 		}
