c4612dfa2f5b00aeb5b6c0e39dfc47c66bbb6ec5
tausiq2003 <tausiqsamantaray03@gmail.com>
2026-01-03 03:08:04 +0530

    echo logic changed

--- app/echo.go
+++ app/echo.go
@@ -7,27 +7,55 @@ import (
 )
 
 func Echo(cmdList []string) (string, error) {
-
 	content := fmt.Sprint(cmdList[1:])
 	realContent := content[1 : len(content)-1]
-	//handle single quote and double quotes, ig they work same,else write
 
-	if (realContent[0] == '\'' && realContent[len(realContent)-1] == '\'') || (realContent[0] == '"' && realContent[len(realContent)-1] == '"') {
-		// just going with the tests, there is another case, if you type one " or '  where > prompts
-		trimmedContent := realContent[1 : len(realContent)-1]
-		trimmedContent = strings.ReplaceAll(trimmedContent, "'", "")
-		trimmedContent = strings.ReplaceAll(trimmedContent, `"`, "")
-		//proceed
+	opened := false
+	//doubleOpened := false
+	var result string
+	var quotesContent string
+	var withoutQuotesContent string
+	for _, v := range realContent {
+		if v == '\'' {
+			//fmt.Println("bar")
+			opened = !opened
 
-		return trimmedContent, nil
+		}
+		//		if v == 39 {
+		//			doubleOpened = !doubleOpened
+		//		}
+		if opened && v != '\'' {
+			if withoutQuotesContent != "" {
 
+				withoutQuotesContent = strings.Join(strings.Fields(withoutQuotesContent), " ")
+				result += withoutQuotesContent
+				withoutQuotesContent = ""
+			}
+			quotesContent += string(rune(v))
+		} else if !opened {
+
+			if v == '\'' {
+				// then it is just closed
+				result += quotesContent
+				quotesContent = ""
+				//				fmt.Printf("--->%v\n", result)
+				//				fmt.Println("baz")
+
+			} else {
+				//treat it as normal thing
+				withoutQuotesContent += string(rune(v))
+				//				fmt.Println("foo")
+				// fmt.Println(withoutQuotesContent)
+			}
+
+		}
 	}
+	fmt.Println(quotesContent)
+	fmt.Printf("%q\n", withoutQuotesContent)
 
-	realContent = strings.ReplaceAll(realContent, "'", "")
-	realContent = strings.ReplaceAll(realContent, `"`, "")
-
-	realContentWithoutQuotes := strings.Join(strings.Fields(realContent), " ")
-
-	return realContentWithoutQuotes, nil
+	withoutQuotesContent = strings.Join(strings.Fields(withoutQuotesContent), " ")
+	result += withoutQuotesContent
+	result = string(result)
+	return result, nil
 
 }
